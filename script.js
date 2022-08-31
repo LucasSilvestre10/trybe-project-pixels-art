e33121024bbf02b1ca701a0a8a8157e52220116e
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2022-08-31 20:07:54 -0300

    requisito 6 feito

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-window.onload = function reload() {
-  if (localStorage.getItem('colorPalette')) {
-    const color = JSON.parse(localStorage.getItem('colorPalette'));
-    const box = document.querySelectorAll('.color');
-    for (let index = 1; index < box.length; index += 1) {
-      box[index].style.backgroundColor = `${color[index - 1]}`;
-    }
-  }
-};
 const button = document.querySelector('button');
 
 function generateColor() {
@@ -29,3 +20,27 @@ function buttonRandomColors() {
   localStorage.setItem('colorPalette', JSON.stringify(colors));
 }
 button.onclick = buttonRandomColors;
+
+function reload() {
+  if (localStorage.getItem('colorPalette')) {
+    const color = JSON.parse(localStorage.getItem('colorPalette'));
+    const box = document.querySelectorAll('.color');
+    for (let index = 1; index < box.length; index += 1) {
+      box[index].style.backgroundColor = `${color[index - 1]}`;
+    }
+  }
+}
+
+function whiteBoard() {
+  const board = document.querySelector('#pixel-board');
+  for (let index = 0; index < 25; index += 1) {
+    const box = document.createElement('div');
+    box.classList.toggle('pixel');
+    board.appendChild(box);
+  }
+}
+
+window.onload = function load() {
+  reload();
+  whiteBoard();
+};
