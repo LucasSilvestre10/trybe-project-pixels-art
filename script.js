835e1f09d2bbd3b465879591276060d9769f5a4a
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2022-09-01 19:00:01 -0300

    requisito 9 e 10 feitos

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const button = document.querySelector('button');
+let selectColor = 'black';
 
 function generateColor() {
   const r = Math.floor(Math.random() * 255);
@@ -17,6 +18,10 @@ function buttonRandomColors() {
     box[index].style.backgroundColor = `${color}`;
     colors.push(color);
   }
+  const selectR = document.getElementsByClassName('selected');
+  selectR[0].classList.remove('selected');
+  const select = document.getElementsByClassName('black');
+  select[0].classList.add('selected');
   localStorage.setItem('colorPalette', JSON.stringify(colors));
 }
 button.onclick = buttonRandomColors;
@@ -43,6 +48,34 @@ function whiteBoard() {
       line.appendChild(box);
     }
   }
+}
+
+function selectedColor(event) {
+  const name = event.target.classList;
+  const style = event.target.style.backgroundColor;
+  if (name.value.includes('selected')) {
+    console.log('já selecionado!', selectColor);
+  } else if (style) {
+    const selectR = document.getElementsByClassName('selected');
+    selectR[0].classList.remove('selected');
+    const select = document.getElementsByClassName(name.value);
+    select[0].classList.add('selected');
+    selectColor = style;
+  } else {
+    const selectR = document.getElementsByClassName('selected');
+    selectR[0].classList.remove('selected');
+    const select = document.getElementsByClassName(name.value);
+    select[0].classList.add('selected');
+    const color = name[1];
+    selectColor = color;
+  }
+}
+
+function paint(event) {
+  console.log(selectColor);
+  console.log(event.target);
+  const target = event.target;
+  target.style.backgroundColor = `${selectColor}`;
 }
 
 window.onload = function load() {
